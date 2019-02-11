909ae363773b67c6d6b5d3a50cc81ef30e1377f1
Julio Daniel Fernandez Aguirre <danielfernandezaguirre@outlook.com>
2019-02-10 23:32:22 -0600

    basic layout

--- public/scripts/main.bundle.js
+++ public/scripts/main.bundle.js
@@ -1,5 +1,29 @@
 'use strict';
 
+function sameConversion(value) {
+	return value;
+}
+
 (function main() {
-	alert('convertidor');
+	var selectFrom = document.querySelector('#select-from');
+	var selectTo = document.querySelector('#select-to');
+	var iFrom = document.querySelector('#i-from');
+	var iTo = document.querySelector('#i-to');
+	var inputFrom = document.querySelector('#input-from');
+	var buttonConvert = document.querySelector('#button-convert');
+	var labelResult = document.querySelector('#label-result');
+	selectFrom.addEventListener('change', function (e) {
+		iFrom.textContent = e.target.options[e.target.selectedIndex].label;
+	});
+	selectTo.addEventListener('change', function (e) {
+		iTo.textContent = e.target.options[e.target.selectedIndex].label;
+	});
+	buttonConvert.addEventListener('click', function () {
+		var currentFrom = parseInt(selectFrom.value);
+		var currentTo = parseInt(selectTo.value);
+		if (currentFrom === currentTo) {
+			var result = sameConversion(parseFloat(inputFrom.value));
+			labelResult.textContent = result + ' ' + selectTo.options[selectTo.selectedIndex].label;
+		}
+	});
 })();
--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function sameConversion(value) {
+	return value;
+}
+
 (function main() {
-	alert('convertidor');
+	const selectFrom = document.querySelector('#select-from');
+	const selectTo = document.querySelector('#select-to');
+	const iFrom = document.querySelector('#i-from');
+	const iTo = document.querySelector('#i-to');
+	const inputFrom = document.querySelector('#input-from');
+	const buttonConvert = document.querySelector('#button-convert');
+	const labelResult = document.querySelector('#label-result');
+	selectFrom.addEventListener('change', e => {
+		iFrom.textContent = e.target.options[e.target.selectedIndex].label;
+	});
+	selectTo.addEventListener('change', e => {
+		iTo.textContent = e.target.options[e.target.selectedIndex].label;
+	});
+	buttonConvert.addEventListener('click', () => {
+		const currentFrom = parseInt(selectFrom.value);
+		const currentTo = parseInt(selectTo.value);
+		if (currentFrom === currentTo) {
+			const result = sameConversion(parseFloat(inputFrom.value));
+			labelResult.textContent = `${result} ${selectTo.options[selectTo.selectedIndex].label}`;
+		}
+	});
 })();
